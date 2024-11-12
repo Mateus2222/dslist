cb95598726565eb0299087a95ec8398e9a37dea6
Mateus2222 <mateuspolari@gmail.com>
2024-11-12 18:07:28 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class GameListService {
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll(){
+        return gameListRepository.findAll().stream().map(x -> new GameListDTO(x)).toList();
+    }
+}
